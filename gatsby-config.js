9fe550c19bbfe5f4999a6f305a39d1b7ce168135
Andy Cochran <acochran@planning.nyc.gov>
2018-01-10 12:04:56 -0500

    port over the labs-planninglabs-home Gatsby site

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: `NYC GeoSearch API`,
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`
+  ],
+}
---
+++ gatsby-node.js
@@ -0,0 +1,20 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+const Promise = require(`bluebird`)
+const path = require(`path`)
+
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+exports.onCreatePage = ({ page, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+  return new Promise((resolve, reject) => {
+    // Make the front page match everything client side.
+    // Normally your paths should be a bit more judicious.
+    createPage(page)
+    resolve()
+  })
+}

4a8e90dd26e63250c0779124a3f60331baca608c
Andy Cochran <acochran@planning.nyc.gov>
2018-01-10 13:02:21 -0500

    update site header

--- src/components/header.js
+++ src/components/header.js
@@ -26,26 +26,22 @@ class Header extends React.Component {
 
     return (
       <header role="banner" className="site-header">
-       <a className="show-on-focus" href="#main">Skip to main content</a>
-        <div className="grid-container">
-          <div className="grid-x grid-padding-x">
-            <div className="cell shrink large-auto">
-              <Link to="/" className="site-logo montserrat">
-                <img src="https://raw.githubusercontent.com/NYCPlanning/logo/master/dcp_logo_772.png" alt="NYC Planning" />
-                <span className="site-logo--l">L</span>
-                <span className="site-logo--a">a</span>
-                <span className="site-logo--b">b</span>
-                <span className="site-logo--s">s</span>
-              </Link>
-            </div>
-            <div className="cell auto hide-for-large text-right">
-              <button className="responsive-nav-toggler" onClick={() => { this.toggleMenu() }}>Menu</button>
-            </div>
-            <div id="responsive-menu" onClick={() => {this.closeMenu()}} className={`cell large-shrink ${menuOpen ? '' : 'show-for-large'}`}>
-              <nav role="navigation" className="header-nav">
-                <NavLinks ulClasses="menu vertical large-horizontal" />
-              </nav>
-            </div>
+        <a className="show-on-focus" href="#main">Skip to main content</a>
+        <a href="https://planninglabs.nyc/" className="labs-beta-notice hide-for-print">A beta project of NYC Planning Labs</a>
+        <div className="grid-x grid-padding-x">
+          <div className="branding cell shrink large-auto">
+            <a className="dcp-link" href="http://www1.nyc.gov/site/planning/index.page"><img className="dcp-logo" src="https://raw.githubusercontent.com/NYCPlanning/logo/master/dcp_logo_772.png" alt="NYC Planning" /></a>
+            <Link to="/" className="site-name">
+              GeoSearch <small className="site-subtitle show-for-medium">Autocomplete Search Results for N<span className="show-for-large">ew </span>Y<span className="show-for-large">ork </span>C<span className="show-for-large">ity</span></small>
+            </Link>
+          </div>
+          <div className="cell auto hide-for-large text-right">
+            <button className="responsive-nav-toggler" onClick={() => { this.toggleMenu() }}>Menu</button>
+          </div>
+          <div id="responsive-menu" onClick={() => {this.closeMenu()}} className={`cell large-shrink ${menuOpen ? '' : 'show-for-large'}`}>
+            <nav role="navigation" className="header-nav">
+              <NavLinks ulClasses="menu vertical large-horizontal" />
+            </nav>
           </div>
         </div>
       </header>

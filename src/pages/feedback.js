45d237dd33e4fdd48a3263b74a066564ba67156f
Andy Cochran <acochran@planning.nyc.gov>
2018-01-22 13:14:03 -0500

    add feedback page with Airtable form

---
+++ src/pages/feedback.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const AboutPage = () => (
+  <div className="page">
+
+    <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js" />
+    <iframe
+      className="airtable-embed airtable-dynamic-height"
+      title="create-idea-form"
+      src="https://airtable.com/embed/shrDlHV5nDpQnXRue?backgroundColor=yellow"
+      frameBorder="0"
+      width="100%"
+      height="850"
+    />
+
+  </div>
+)
+
+export default AboutPage

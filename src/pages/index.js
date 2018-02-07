68fde70a2a453ebb38816d9bb8c69c7901652711
Andy Cochran <acochran@planning.nyc.gov>
2018-02-07 11:19:51 -0500

    add beta warning to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,11 @@ class IndexPage extends React.Component {
     return (
       <div className="page">
 
+        <div className="callout secondary">
+          <p className="header-tiny" style={{margin:'0 0 0.5em'}}><FontAwesome name='exclamation-circle' fixedWidth className='warning-color' style={{transform:'scale(1.5)'}} />&nbsp; <strong>NYC GeoSearch is currently in beta</strong></p>
+          <p className="text-small">We're currently testing GeoSearch in various applications and may make unannounced changes. While we welcome you to experiment with this service and <Link to="feedback/"><strong>provide feedback</strong></Link>, it is not yet supported by the <a href="http://nyc.gov/planning">Department of City Planning</a>.</p>
+        </div>
+
         <p className="lead">NYC GeoSearch is a service that transforms input text—such as an address, or the name of a place—to authoritative New York City addresses using data from the <a href="https://www1.nyc.gov/site/planning/data-maps/open-data.page#pad">Property Address Directory</a>. </p>
 
         <div className="grid-x grid-margin-x grid-padding-y">

39258d1a92a627b8406b34f947924cf50e08c5a6
Andy Cochran <acochran@planning.nyc.gov>
2018-02-07 11:41:19 -0500

    add github link to feedback page

--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -4,6 +4,7 @@ import FontAwesome from 'react-fontawesome';
 
 const Loading = props => (
   <div>
+
     { props.loading &&
       (
         <div className="text-center dark-gray" style={{ margin: '80px 0', padding: '20px 0', position: 'relative' }} >
@@ -45,6 +46,9 @@ class AboutPage extends Component {
   render() {
     const form = (
       <div className="page">
+
+        <p className="text-right text-small no-margin"><a href="https://github.com/NYCPlanning/labs-geosearch-dockerfiles/issues/new" target="_blank"><FontAwesome name='github' fixedWidth style={{transform:'scale(1.5)'}} />&nbsp;<strong>Create an issue</strong></a></p>
+
         <Loading loading={this.state.loading} />
         <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js" />
         <iframe

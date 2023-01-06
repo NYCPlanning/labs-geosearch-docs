75fccc7a67c859f4063c9f644fa69fe9f71749f7
Chris Whong <chris.m.whong@gmail.com>
2023-01-06 12:40:42 -0500

    update search component to use v2 url, change references to v1 in docs pages

--- src/components/map.js
+++ src/components/map.js
@@ -5,7 +5,7 @@ import Search from './search';
 import Response from '../components/response';
 
 let MapboxGl = null;
-const apiCallNullMessage = 'http://159.89.238.220/v1/autocomplete?text='
+const apiCallNullMessage = 'http://159.89.238.220/v2/autocomplete?text='
 const responseNullMessage = '{}'
 
 class Map extends React.Component {
